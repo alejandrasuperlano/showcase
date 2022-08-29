d5dfd4240f76a89dc5ee28cae32e6597d48a8a45
mbolanoss <mbolanoss@unal.edu.co>
2022-08-28 21:15:04 -0500

    Added kernel selection

--- content/sketches/imageKernel.js
+++ content/sketches/imageKernel.js
@@ -1,13 +1,20 @@
 new p5((p) => {
-  let img;
+  let originalImg;
+  let currentImg;
 
-  let kernel = [
+  let noKernel = [
+    [0, 0, 0],
+    [0, 1, 0],
+    [0, 0, 0],
+  ];
+
+  let outlineKernel = [
     [-1, -1, -1],
     [-1, 8, -1],
     [-1, -1, -1],
   ];
 
-  let applyKernel = (x, y, kernelSize) => {
+  let applyKernelToPixel = (x, y, kernel, kernelSize) => {
     let newR = 0.0;
     let newG = 0.0;
     let newB = 0.0;
@@ -21,12 +28,12 @@ new p5((p) => {
         let imageCol = y - colOffset;
 
         if (
-          imageRow > 0 &&
-          imageRow < img.width &&
-          imageCol > 0 &&
-          imageCol < img.height
+          imageRow >= 0 &&
+          imageRow < currentImg.width &&
+          imageCol >= 0 &&
+          imageCol < currentImg.height
         ) {
-          let currentPixel = img.get(imageRow, imageCol);
+          let currentPixel = originalImg.get(imageRow, imageCol);
 
           newR += p.red(currentPixel) * kernel[r][c];
           newG += p.green(currentPixel) * kernel[r][c];
@@ -35,29 +42,48 @@ new p5((p) => {
       }
     }
 
-    return p.color(newR % 255, newG % 255, newB % 255);
+    newR = p.constrain(newR, 0, 255);
+    newG = p.constrain(newG, 0, 255);
+    newB = p.constrain(newB, 0, 255);
+    return p.color(newR, newG, newB);
   };
 
   p.preload = function () {
-    img = p.loadImage("/showcase/sketches/animal.jpg");
+    originalImg = p.loadImage("/showcase/sketches/animal.jpg");
+    currentImg = p.loadImage("/showcase/sketches/animal.jpg");
   };
 
   p.setup = function () {
-    p.createCanvas(img.width, img.height);
-    img.loadPixels();
-
-    for (let x = 0; x < img.width; x++) {
-      for (let y = 0; y < img.height; y++) {
-        let newPixel = applyKernel(x, y, 3);
-        img.set(x, y, newPixel);
-      }
-    }
-    img.updatePixels();
+    p.createCanvas(originalImg.width, originalImg.height);
+    p.noLoop();
   };
 
   p.draw = function () {
     p.imageMode(p.CENTER);
+    p.image(currentImg, currentImg.width / 2, currentImg.height / 2);
+  };
 
-    p.image(img, img.width / 2, img.height / 2);
+  p.keyTyped = function () {
+    if (p.key == "a") {
+      for (let x = 0; x < currentImg.width; x++) {
+        for (let y = 0; y < currentImg.height; y++) {
+          let newPixel = applyKernelToPixel(x, y, noKernel, 3);
+          currentImg.set(x, y, newPixel);
+        }
+      }
+
+      currentImg.updatePixels();
+      p.redraw();
+    } else if (p.key == "b") {
+      for (let x = 0; x < currentImg.width; x++) {
+        for (let y = 0; y < currentImg.height; y++) {
+          let newPixel = applyKernelToPixel(x, y, outlineKernel, 3);
+          currentImg.set(x, y, newPixel);
+        }
+      }
+
+      currentImg.updatePixels();
+      p.redraw();
+    }
   };
 }, "imageKernel");
