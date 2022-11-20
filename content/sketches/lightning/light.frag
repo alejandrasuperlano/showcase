1cbe74f0b30272f07f7f1368db463dcd50be7f8e
mbolanoss <mbolanoss@unal.edu.co>
2022-11-19 19:26:14 -0500

    Added colored ambient light

--- content/sketches/lightning/light.frag
+++ content/sketches/lightning/light.frag
@@ -3,9 +3,10 @@ precision mediump float;
 // emitted by p5 color-group commands
 // https://p5js.org/reference/#group-Color
 uniform vec4 uMaterialColor;
-
+uniform vec4 lightColor;
 uniform float ambient;
 
 void main() {
-  gl_FragColor = ambient * uMaterialColor;
+  vec4 ambient4 = lightColor * ambient;
+  gl_FragColor = ambient4 * uMaterialColor;
 }
--- content/sketches/lightning/sketch.js
+++ content/sketches/lightning/sketch.js
@@ -1,10 +1,13 @@
-let easycam;
 let myShader;
-let ambient;
+let models;
+
+let ambientSlider;
+let modelsSlider;
+let colorPicker;
 
 function preload() {
   myShader = readShader("/showcase/sketches/lightning/light.frag", {
-    varyings: Tree.texcoords2,
+    varyings: Tree.NONE,
   });
 }
 
@@ -14,19 +17,76 @@ function setup() {
   colorMode(RGB, 1);
   setAttributes("antialias", true);
 
-  ambient = createSlider(0, 1, 0.5, 0.05);
-  ambient.position(420, 10);
-  ambient.style("width", "80px");
-  ambient.input(() => {
-    myShader.setUniform("ambient", ambient.value());
-  });
+  let trange = 100;
+  models = [];
+
+  for (let i = 0; i < 100; i++) {
+    models.push({
+      position: createVector(
+        (random() * 2 - 1) * trange,
+        (random() * 2 - 1) * trange,
+        (random() * 2 - 1) * trange
+      ),
+      size: random() * 25 + 8,
+      color: color(random(), random(), random()),
+    });
+  }
+
+  setupUI();
 
   shader(myShader);
-  myShader.setUniform("ambient", ambient.value());
+  myShader.setUniform("ambient", ambientSlider.value());
+  myShader.setUniform("lightColor", [1, 1, 1, 1]);
 }
 
 function draw() {
-  background(0);
   orbitControl();
-  box(100);
+  background(0);
+  resetShader();
+  push();
+  stroke("green");
+  axes();
+  grid();
+  pop();
+  shader(myShader);
+  for (let i = 0; i < modelsSlider.value(); i++) {
+    push();
+    noStroke();
+    fill(models[i].color);
+    translate(models[i].position);
+    let radius = models[i].size / 2;
+    i % 3 === 0
+      ? box(radius * 2)
+      : i % 3 === 1
+      ? sphere(radius)
+      : torus(radius, radius / 4);
+    pop();
+  }
+}
+
+function setupUI() {
+  let modelsTitle = createP("Choose models to display:");
+  modelsTitle.style("font-weight", "bold");
+  modelsSlider = createSlider(1, models.length, int(models.length / 4), 1);
+
+  let ambientTitle = createP("Choose lighting level:");
+  ambientTitle.style("font-weight", "bold");
+  ambientSlider = createSlider(0, 1, 0.5, 0.05);
+  ambientSlider.input(() => {
+    myShader.setUniform("ambient", ambientSlider.value());
+  });
+
+  let colorTitle = createP("Choose light color:");
+  colorTitle.style("font-weight", "bold");
+  colorPicker = createColorPicker("#FFFFFF");
+  colorPicker.input(() => {
+    let color = colorPicker.color();
+
+    myShader.setUniform("lightColor", [
+      red(color) / 255,
+      green(color) / 255,
+      blue(color) / 255,
+      1,
+    ]);
+  });
 }
